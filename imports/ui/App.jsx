02cff18af94eeb535f984bcfb49d5bf38fd5947f
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2021-07-27 23:56:56 +0800

    Task Update Checked and Remove Task

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -18,7 +18,23 @@ export const App = () => {
   //   TasksCollection.find({}).fetch()
   // });
 
-  const tasks = useTracker( () => TasksCollection.find({},  { sort: { createdAt: -1 } }).fetch());
+  const tasks = useTracker( () => 
+    TasksCollection
+      .find({},  { sort: { createdAt: -1 } })
+      .fetch()
+    );
+
+  const toggleCheckbox= ({ _id, isChecked }) => {
+    TasksCollection.update( _id, {
+      $set: {
+        isChecked: !isChecked
+      }
+    })
+  }
+
+  const deleteTask = ({ _id }) => {
+    TasksCollection.remove(_id)
+  }
 
   return (
     <div>
@@ -29,7 +45,13 @@ export const App = () => {
       <TaskForm />
 
       <ul>
-        { tasks.map( task => <Task key={task._id} task={task}/>) }
+        { tasks.map( task => 
+          <Task 
+            key={task._id}
+            task={task} 
+            onCheckboxClick={toggleCheckbox} 
+            onDeleteClick={deleteTask}/>
+        )}
       </ul> 
     
     </div>
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 
-export const Task = ({ task }) => {
-    return <li className="task">{task.name}</li>
+export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
+    return (
+        <li className="task">
+            <div>
+                <input 
+                type="checkbox"
+                checked={task.isChecked}  
+                onClick={ () => onCheckboxClick(task) }
+                readOnly />
+
+                <span style={{'paddingLeft': '1rem'}}>{ task.name }</span>
+            </div>
+
+            <div>
+                <a onClick={() => onDeleteClick(task)}>
+                    <svg className="w-2 h-2" data-darkreader-inline-fill="" fill="currentColor" style={{"--darkreaderInlineFill": "currentColor"}} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd"></path></svg>
+                </a>
+            </div>
+        </li>
+    )
 }
 
