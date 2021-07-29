7b5b6712f0732f9dc15fa94d9032c6903c44409b
Carl Justine Mosquida <mosquidacarljustine@gmail.com>
2021-07-29 23:23:34 +0800

    Username CSS Layout

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -75,13 +75,21 @@ export const App = () => {
 
   return (
     <>
-      <h1>Meteor Tasks {pendingTaskNumber} </h1>
-      <h3>POWERED BY METEOR X REACT</h3>
+      
       {/* <Hello/> */} 
 
-      <div style={{'display': 'flex', 'justify-content': 'space-between'}}>
-        <h2>{user ? user.username : ''}</h2>
-        <a className="button" onClick={() => Meteor.logout()}>Logout</a>
+      <div className="flex-between">
+        <div>
+          <h1>Meteor Tasks {pendingTaskNumber} </h1>
+          <h3>POWERED BY METEOR X REACT</h3>
+        </div>
+
+        <div className="flex-between">
+          <h2 className="flex-center">@{user ? user.username : ''}</h2>
+          <a className="flex-center" onClick={() => Meteor.logout()}>
+            <svg className="w-4 " data-darkreader-inline-fill="" fill="currentColor" style={{"--darkreader-inline-fill":"currentColor"}} viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd"></path></svg>
+          </a>
+        </div>
       </div>
       
       { user ? (
